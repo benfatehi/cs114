9f20849faf782ddb9e6aab2b1ded80c50bfad7b2
s203 <s203@AI241LAB0203M23.local>
2017-11-01 09:41:22 -0700

    updates

---
+++ magic8ball.py
@@ -0,0 +1,23 @@
+import random
+print("Hello there, welcome to Russian Magic 8 Ball. \nGive me your name, NOW!.")
+name=input()
+number = random.randint(1,9)
+if number==1:
+    print("You will be rich, Russian rich.")
+elif number==2:
+    print("You will be sloth, Russian sloth.")
+elif number==3:
+    print("You will be jerk, Russian jerk.")
+elif number==4:
+    print("You will be good person, wait Russians aren't good people..")
+elif number==5:
+    print("You will be......")
+elif number==6:
+    print("You will be help those who need it...... nah. Screw that. ")
+elif number==7:
+    print("You will be cancer.")
+elif number==8:
+    print("You will be troll.")
+elif number==9:
+    print("You will die tomorrow.")
+print("Bye have a beautiful time.")
